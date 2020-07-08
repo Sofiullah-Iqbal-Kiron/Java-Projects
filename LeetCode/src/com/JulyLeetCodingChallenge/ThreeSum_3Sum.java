4fe3b68c26306ce8ea72af1fb448123479a9bf7e
Sofiullah Iqbal Kiron <sofiul.k.1023@gmail.com>
2020-07-08 18:42:17 +0600

    No commit

---
+++ LeetCode/src/com/JulyLeetCodingChallenge/ThreeSum_3Sum.java
@@ -0,0 +1,24 @@
+package com.JulyLeetCodingChallenge;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum_3Sum
+{
+    public static void main(String[] args)
+    {
+        threeSum(new int[] {1});
+    }
+    public static List<List<Integer>> threeSum(int[] nums)
+    {
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> subList=new ArrayList<>();
+
+        list.add(Arrays.asList(1, 2,3));
+        list.add(Arrays.asList(5, 4, 3));
+        System.out.println(list);
+
+        return list;
+    }
+}
