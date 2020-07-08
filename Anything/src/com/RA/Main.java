6e2ccfb32570f1187d263ad907d41669cfc0abc8
Sofiullah Iqbal Kiron <sofiul.k.1023@gmail.com>
2020-07-08 18:12:31 +0600

    Mazak with Main

--- Anything/src/com/RA/Main.java
+++ Anything/src/com/RA/Main.java
@@ -9,9 +9,9 @@ public class Main
     public static void main(String[] args)
     {
         // write your code here
-        int x;
+        /*int x;
         x = input.nextInt();
-        System.out.println(x);
+        System.out.println(x);*/
     }
 
     public static void newLine()
