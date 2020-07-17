bf299c6cd7917aa0eafd35f891ce82a781d1a3bb
Sofiullah Iqbal Kiron <sofiul.k.1023@gmail.com>
2020-07-17 08:27:48 +0600

    Challenge for 50 completed

---
+++ LeetCode/src/com/JulyLeetCodingChallenge/PowOfxn.java
@@ -0,0 +1,14 @@
+// Accepted but that was not expected.
+// We need to implement in own way.
+// Posted in twitter.
+// Ubuntu-Link:
+
+package com.JulyLeetCodingChallenge;
+
+public class PowOfxn
+{
+    public double myPow(double x, int n)
+    {
+        return Math.pow(x, n);
+    }
+}
---
+++ LeetCode/src/com/LeetCode/SqrtOfX_69.java
@@ -0,0 +1,13 @@
+// Accepted.
+// Posted in twitter.
+// Ubuntu-Link: https://pastebin.ubuntu.com/p/FsnB6yt9GC/
+
+package com.LeetCode;
+
+public class SqrtOfX_69
+{
+    public int mySqrt(int x)
+    {
+        return (int) Math.sqrt(x);
+    }
+}
