484d32c8dc85d2361ba861415fca5bbbd0de571d
Sofiullah Iqbal Kiron <sofiul.k.1023@gmail.com>
2021-01-09 18:16:00 +0600

    Modified Factorial Calculator

--- Anything/src/com/RA/GUI/FactorialCalculator.java
+++ Anything/src/com/RA/GUI/FactorialCalculator.java
@@ -2,6 +2,7 @@
 
 // Press (Ctrl + Y) to delete full line in current caret.
 
+
 package com.RA.GUI;
 
 import javax.swing.*;
@@ -9,79 +10,7 @@ import javax.swing.border.Border;
 import java.awt.*;
 import java.math.BigInteger;
 
-public class FactorialCalculator
-{
-    private static JLabel number = new JLabel("Number: "), Ans = new JLabel(""), factorial =
-            new JLabel("Nothing to " + "show"), thanksLabel = new JLabel("");
-
-    public static void main(String[] args)
-    {
-        JFrame frame = new JFrame("Calculate Factorial");
-        JTextField textField = new JTextField(10);
-
-        Container contentPane = frame.getContentPane();
-
-        JButton CloseApp = new JButton("Close App");
-        JButton enter = new JButton("Enter");
-        enter.setBackground(Color.gray);
-        enter.addActionListener(e -> {
-            contentPane.remove(CloseApp);
-            try
-            {
-                if (textField.getText().equals(""))
-                {
-                    enter.setBackground(Color.red);
-                    Ans.setText("");
-                    factorial.setText("");
-                    thanksLabel.setText("Please Enter A Number");
-                }
-                else
-                {
-                    enter.setBackground(Color.green);
-                    Ans.setText("Factorial of " + Integer.parseInt(textField.getText()) + '\u0020' + '\u003A' + " ");
-                    factorial.setText(fact(Integer.parseInt(textField.getText())));
-                    thanksLabel.setText("Thanks for using our Application");
-                }
-            }
-            catch (NumberFormatException numberFormatException)
-            {
-                enter.setBackground(Color.yellow);
-                Ans.setText("");
-                factorial.setText("");
-                thanksLabel.setText("Invalid Input");
-            }
-
-            CloseApp.addActionListener(E -> {System.exit(0);});
-            contentPane.add(CloseApp);
-        });
-
-        contentPane.add(number);
-        contentPane.add(textField);
-        contentPane.add(enter);
-        contentPane.add(Ans);
-        contentPane.add(factorial);
-        contentPane.add(thanksLabel);
-        contentPane.setLayout(new FlowLayout());
-
-        frame.setBackground(Color.green);
-        frame.setSize(new Dimension(300, 180));
-        frame.setLayout(new FlowLayout());
-        frame.setVisible(true);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-    }
-
-    private static String fact(int number)
-    {
-        BigInteger fact = new BigInteger("1");
-        for (int i = 2; i <= number; i++)
-            fact = fact.multiply(BigInteger.valueOf(i));
-
-        return fact.toString();
-    }
-}
-
-class FactorialCalculator2
+class FactorialCalculator
 {
     //    Fields
     public static JFrame mainFrame = new JFrame("Calculate Your Factorial");
@@ -125,10 +54,52 @@ class FactorialCalculator2
         return leftPanel;
     }
 
-    public static JPanel buildCenterPanel()
+    private static JPanel buildCenterPanel()
     {
+//        Declaration.
         JPanel centerPanel = new JPanel();
+
+//        Undone.
+        JPanel northPanelForCenterPanel = new JPanel();
+
+        JLabel enterMessageLabel = new JLabel("Enter Your number: ");
+        JTextField enterTextField = new JTextField(10);
+        JLabel ansLabel = new JLabel("Welcome, Nothing To Show");
+        JButton enterButton = new JButton("Enter");
+        JButton closeButton = new JButton("Close");
+
+//        Adding Listeners.
+        enterButton.addActionListener(e -> {
+            try
+            {
+                if (enterTextField.getText().equals(""))
+                {
+                    enterButton.setBackground(Color.red);
+                    ansLabel.setText("Empty! Enter A Valid Number");
+                }
+                else
+                {
+                    enterButton.setBackground(Color.green);
+                    ansLabel.setText("Factorial of " + Integer.parseInt(enterTextField.getText()) + ": " + factorialAns(Integer.parseInt(enterTextField.getText())));
+                }
+            }
+            catch (NumberFormatException numberFormatException)
+            {
+                enterButton.setBackground(Color.red);
+                ansLabel.setText("Invalid Input. Enter A Valid Integer");
+            }
+        });
+
+        closeButton.addActionListener(e -> System.exit(0));
+
+//        Modifying center panel.
         centerPanel.setBorder(centerPanelBorder());
+        centerPanel.setLayout(new FlowLayout());
+        centerPanel.add(enterMessageLabel);
+        centerPanel.add(enterTextField);
+        centerPanel.add(enterButton);
+        centerPanel.add(ansLabel);
+        centerPanel.add(closeButton);
 
         return centerPanel;
     }
