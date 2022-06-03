898f5f9e288082003dd1456d40e420e4093e9ee9
Sofiullah Iqbal Kiron <sofiul.k.1023@gmail.com>
2022-06-03 12:05:10 +0600

    CodeChef First, accepted in first term.

---
+++ Online Judge/src/CodeChef/My_very_1st_contest.java
@@ -0,0 +1,14 @@
+// Accepted in first term.
+// Just a silly question.
+
+package CodeChef;
+
+import java.util.Scanner;
+
+public class My_very_1st_contest {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt(), A = input.nextInt(), B = input.nextInt();
+        System.out.println((N - A) + " " + (N - A - B));
+    }
+}
